23e62833add5d8e918e6b56937c710d175971812
John Larsen <jr.larsen@mac.com>
2020-04-19 13:54:09 +0100

    add changeBookable function and try to change bookableIndex directly

--- src/components/Bookables.js
+++ src/components/Bookables.js
@@ -1,13 +1,15 @@
-import React from "react";
+import React from 'react';
 import {bookables} from "../db.json";
 
 export default function Bookables () {
-
   const group = "Rooms";
-
   const bookablesInGroup = bookables.filter(b => b.group === group);
+  let bookableIndex = 1;
 
-  const bookableIndex = 1;
+  function changeBookable (selectedIndex) {
+    bookableIndex = selectedIndex;
+    console.log(selectedIndex);
+  }
 
   return (
     <ul className="bookables">
@@ -15,6 +17,7 @@ export default function Bookables () {
         <li
           key={b.title}
           className={i === bookableIndex ? "selected" : null}
+          onClick={() => changeBookable(i)}
         >
           {b.title}
         </li>
