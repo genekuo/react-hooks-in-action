e6af6890b6eeb2a468235d89fe1d85935e13d470
John Larsen <jr.larsen@mac.com>
2020-04-19 14:33:13 +0100

    add bookable details UI and ability to toggle details

--- src/components/Bookables.js
+++ src/components/Bookables.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, Fragment } from 'react';
 import {bookables} from "../db.json";
 
 export default function Bookables () {
@@ -7,33 +7,62 @@ export default function Bookables () {
   const [bookableIndex, setBookableIndex] = useState(0);
   const groups = [...new Set(bookables.map(b => b.group))];
 
+  const bookable = bookablesInGroup[bookableIndex];
+
+  const [hasDetails, setHasDetails] = useState(false);
+
   function nextBookable () {
     setBookableIndex(i => (i + 1) % bookablesInGroup.length);
   }
 
   return (
-    <div>
-      <select
-        value={group}
-        onChange={(e) => setGroup(e.target.value)}
-      >
-        {groups.map(g => <option value={g} key={g}>{g}</option>)}
-      </select>
+    <Fragment>
+      <div>
+        <select
+          value={group}
+          onChange={(e) => setGroup(e.target.value)}
+        >
+          {groups.map(g => <option value={g} key={g}>{g}</option>)}
+        </select>
 
-      <ul className="bookables">
-        {bookablesInGroup.map((b, i) => (
-          <li
-            key={b.title}
-            className={i === bookableIndex ? "selected" : null}
-            onClick={() => setBookableIndex(i)}
-          >
-            {b.title}
-          </li>
-        ))}
-      </ul>
-      <p>
-        <button onClick={nextBookable} autoFocus>Next</button>
-      </p>
-    </div>
+        <ul className="bookables">
+          {bookablesInGroup.map((b, i) => (
+            <li
+              key={b.title}
+              className={i === bookableIndex ? "selected" : null}
+              onClick={() => setBookableIndex(i)}
+            >
+              {b.title}
+            </li>
+          ))}
+        </ul>
+        <p>
+          <button onClick={nextBookable} autoFocus>Next</button>
+        </p>
+      </div>
+
+      <div className="bookableDetails">
+        {bookable && (
+          <Fragment>
+            <p>
+              <label>
+                <input
+                  type="checkbox"
+                  onChange={(e) => setHasDetails(e.target.checked)}
+                  checked={hasDetails}
+                />
+                Show Details
+              </label>
+            </p>
+            {hasDetails && (
+              <div>
+                <h2>{bookable.title}</h2>
+                <p>{bookable.notes}</p>
+              </div>
+            )}
+          </Fragment>
+        )}
+      </div>
+    </Fragment>
   );
 }
