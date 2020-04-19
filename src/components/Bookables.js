0a3feb074573a237c7c80e67c4ed34d7a8e289db
John Larsen <jr.larsen@mac.com>
2020-04-19 14:11:01 +0100

    add Next button and associated CSS

--- src/components/Bookables.js
+++ src/components/Bookables.js
@@ -4,19 +4,28 @@ import {bookables} from "../db.json";
 export default function Bookables () {
   const group = "Rooms";
   const bookablesInGroup = bookables.filter(b => b.group === group);
-  const [ bookableIndex, setBookableIndex ] = useState(1);
+  const [ bookableIndex, setBookableIndex ] = useState(0);
+
+  function nextBookable () {
+    setBookableIndex(i => (i + 1) % bookablesInGroup.length);
+  }
 
   return (
-    <ul className="bookables">
-      {bookablesInGroup.map((b, i) => (
-        <li
-          key={b.title}
-          className={i === bookableIndex ? "selected" : null}
-          onClick={() => setBookableIndex(i)}
-        >
-          {b.title}
-        </li>
-      ))}
-    </ul>
+    <div>
+      <ul className="bookables">
+        {bookablesInGroup.map((b, i) => (
+          <li
+            key={b.title}
+            className={i === bookableIndex ? "selected" : null}
+            onClick={() => setBookableIndex(i)}
+          >
+            {b.title}
+          </li>
+        ))}
+      </ul>
+      <p>
+        <button onClick={nextBookable} autoFocus>Next</button>
+      </p>
+    </div>
   );
 }
