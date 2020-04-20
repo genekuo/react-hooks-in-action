a72f7aa7964685b507dcf16958d514c3e34f0fa9
John Larsen <jr.larsen@mac.com>
2020-04-20 10:40:01 +0100

    the code for the WeekPicker component

---
+++ src/components/WeekPicker/index.js
@@ -0,0 +1,16 @@
+import React, {useReducer} from "react";
+import reducer from "./reducer";
+import {getWeek} from "../../utils/date-wrangler";
+
+export default function WeekPicker ({date}) {
+  const [week, dispatch] = useReducer(reducer, date, getWeek);
+
+  return (
+    <p>
+      <button onClick={() => dispatch({type: "PREV_WEEK"})}>Previous</button>
+      <button onClick={() => dispatch({type: "TODAY"})}>Today</button>
+      <button onClick={() => dispatch({type: "NEXT_WEEK"})}>Next</button>
+      {week.start.toDateString()} - {week.end.toDateString()}
+    </p>
+  );
+}
