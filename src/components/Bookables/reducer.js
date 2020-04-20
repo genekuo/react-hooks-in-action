ea1cbf53e725b902907d9434e97541037b2a36b0
John Larsen <jr.larsen@mac.com>
2020-04-20 13:46:30 +0100

    update reducer for data-fetching actions

--- src/components/Bookables/reducer.js
+++ src/components/Bookables/reducer.js
@@ -20,13 +20,32 @@ export default function reducer (state, action) {
       };
 
     case "NEXT_BOOKABLE":
-      const count = state.bookables.filter(
-        b => b.group === state.group
-      ).length;
-
+      const count = state.bookables.filter(b => b.group === state.group).length;
       return {
         ...state,
         bookableIndex: (state.bookableIndex + 1) % count
+      };
+
+    case "FETCH_BOOKABLES_REQUEST":
+      return {
+        ...state,
+        isLoading: true,
+        error: false,
+        bookables: []
+      };
+
+    case "FETCH_BOOKABLES_SUCCESS":
+      return {
+        ...state,
+        isLoading: false,
+        bookables: action.payload
+      };
+
+    case "FETCH_BOOKABLES_ERROR":
+      return {
+        ...state,
+        isLoading: false,
+        error: action.payload
       };
 
     default:
