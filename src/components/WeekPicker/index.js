19cf2149bfbb4fa574c147a67ade5b82e0e5eca7
John Larsen <jr.larsen@mac.com>
2020-04-20 16:31:34 +0100

    include a text box and Go button for the WeekPicker

--- src/components/WeekPicker/index.js
+++ src/components/WeekPicker/index.js
@@ -1,16 +1,32 @@
-import React, {useReducer} from "react";
+import React, {useReducer, useRef} from "react";
 import reducer from "./reducer";
 import {getWeek} from "../../utils/date-wrangler";
 
 export default function WeekPicker ({date}) {
   const [week, dispatch] = useReducer(reducer, date, getWeek);
+  const textboxRef = useRef();
+
+  function goToDate () {
+    dispatch({
+      type: "SET_DATE",
+      payload: textboxRef.current.value
+    });
+  }
 
   return (
-    <p>
-      <button onClick={() => dispatch({type: "PREV_WEEK"})}>Previous</button>
-      <button onClick={() => dispatch({type: "TODAY"})}>Today</button>
-      <button onClick={() => dispatch({type: "NEXT_WEEK"})}>Next</button>
-      {week.start.toDateString()} - {week.end.toDateString()}
-    </p>
+    <div className="datePicker">
+      <p>
+        <button onClick={() => dispatch({type: "PREV_WEEK"})}>Previous</button>
+        <button onClick={() => dispatch({type: "TODAY"})}>Today</button>
+        <button onClick={() => dispatch({type: "NEXT_WEEK"})}>Next</button>
+
+        <input type="text" ref={textboxRef} />
+
+        <button onClick={goToDate}>Go</button>
+      </p>
+      <p>
+        {week.start.toDateString()} - {week.end.toDateString()}
+      </p>
+    </div>
   );
 }
